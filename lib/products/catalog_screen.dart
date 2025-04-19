5bd11812a3cb6ab450083d07019972f532942da3
balfaz <balfaz@gmail.com>
2025-04-19 14:49:24 +0200

    continua la implementazione della interfaccia catalogo dei prodotti, lettura da file json

--- lib/products/catalog_screen.dart
+++ lib/products/catalog_screen.dart
@@ -8,16 +8,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class CatalogScreen extends ConsumerWidget {
   const CatalogScreen({super.key});
 
-  Future<List<Product>> loadJsonProducts() async {
+  Future<List<dynamic>> loadJsonProducts() async {
     final String stringListProducts = await rootBundle.loadString(
-      'assets/products.json',
+      'assets/articoli.json',
     );
     final List<dynamic> jsonList = json.decode(stringListProducts);
-    return jsonList.map((item) => Product.fromMap(item)).toList();
+    return jsonList;
+    //.map((item) => Product.fromMap(item)).toList();
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    //final List<Product> listProducts = loadJsonProducts();
     return Scaffold(
       appBar: AppBar(title: const Text('Catalog')),
       body: SingleChildScrollView(
@@ -27,14 +29,58 @@ class CatalogScreen extends ConsumerWidget {
             const Divider(height: 1),
             const SizedBox(child: Text('Catalog Screen')),
             // Add your catalog items here
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 20, // Example item count
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Product $index'),
-                  subtitle: Text('Description of Product $index'),
+            FutureBuilder(
+              future: loadJsonProducts(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+                if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                List products = snapshot.data!;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 17, // Example item count
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0,
+                        vertical: 4,
+                      ),
+                      child: Card(
+                        child: ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Product: ${products[index]['codice']}'),
+                              Text(
+                                'Price: ${products[index]['listino2']}',
+                                style: TextStyle(
+                                  color: Colors.indigo,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Description',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text('${products[index]['descrizione']}'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
             ),
